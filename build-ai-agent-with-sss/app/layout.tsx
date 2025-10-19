1254e0275120bde62bb2b7a72d0f90238bb3e45a
nik.6348 <nikhil@singaji.in>
2025-10-19 19:07:32 +0530

    update favicon

--- build-ai-agent-with-sss/app/layout.tsx
+++ build-ai-agent-with-sss/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <head>
+        <link rel="icon" href="https://singaji.in/assest/SSS-Favicon-Design.png" sizes="any" />
+        <link rel="apple-touch-icon" href="https://singaji.in/assest/SSS-Favicon-Design.png" sizes="180x180" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
